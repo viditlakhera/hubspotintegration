b8b26af1cddac3efe8f893cbbb917353b91aa79f
viditlakhera <49707088+viditlakhera@users.noreply.github.com>
2023-09-11 06:18:16 +0000

    Pending changes exported from your codespace

--- db.js
+++ db.js
@@ -1,19 +1,5 @@
-const mysql = require('mysql2');
+let database = ['dfsf'];
 
-const dbpool = mysql.createPool({
-  connectionLimit : 50, //important
-//   host: dbconnection.host,
-//   user: dbconnection.user,
-//   password: dbconnection.password,
-//   database: dbconnection.database,
-host: 'testwattmonkdb-do-user-8587296-0.b.db.ondigitalocean.com', // database endpoint
-port: 25060,
-database: 'titansv4', // DB name
-user: 'doadmin', // your username for mysql
-password: 'AVNS_3lzv2XN5b491XBspt-2', // your password for mysql
-  port: dbconnection.port,
-  debug: false,
-  waitForConnections: true,
-  queueLimit: 0
-});
 
+
+module.exports = database;
--- index.js
+++ index.js
@@ -7,57 +7,36 @@ const config = require('./config/config');
 const db = require('./config/titans');
 const path = require('path');
 const bodyParser = require('body-parser');
-
+const ejs = require('ejs');
+const databasefile = require('./db')
+const initialdetail = require("./initaldetail")
 app.use(bodyParser.urlencoded({ extended: true }));
-// create the connection......................
-// const pool = mysql.createPool({
-//   host: "testwattmonkdb-do-user-8587296-0.b.db.ondigitalocean.com",
-//   user: "doadmin",
-//   password: "AVNS_3lzv2XN5b491XBspt-2",
-//   database: "sunnovav4",
-//   port: 25060,
-//    debug: false,
-//    connectionLimit : 1000,
-//    waitForConnections:true,
-//    queueLimit:0
-// });
-// pool.getConnection((err,connection)=> {
-//   console.log(connection)
-//   if(err){
-//     console.log(err)
-//     throw err;
-//   } 
-//   console.log('Database connected successfully');
-//   connection.release();
-// });
+
+//set ejs as view engine
+app.set('view engine', 'ejs');
+
+const responsedata = [];
+
+app.post('/ok', async(req,res)=>{
+  console.log("req",req.body);
+  res.send(req.body);
+ })
 
 app.get('/api/form', async (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-  //   const options = {
-  //     root: path.join(__dirname)
-  // };
-
-  // const fileName = 'app.html';
-  // res.sendFile(fileName, options, function (err) {
-  //     if (err) {
-  //         next(err);
-  //     } else {
-  //         console.log('Sent:', fileName);
-  //     }
-  // });
 });
 
 app.post('/submit', async (req, res) => {
   console.log('hitting.............')
   console.log("req", req.body);
 
-  const object = req.body;
-
-  let propertiesinarray = Object.values(object);
+  const json_object = req.body;
+    
+  let propertiesinarray = Object.values(json_object);
   console.log("propertiesinarray", propertiesinarray)
   let joinpropertiesinarray = propertiesinarray.join()
   console.log("join", joinpropertiesinarray);
-  const access_token = "CMmfg6OnMRIMAAEAQAAAYQIAAAAYGL-G5hQg5tDIHCjLvnwyFAZAsL8ar7K4Ql9cLHGATn5V17E8OjAAAABBAAAAAAAAAAAAAAAAAIAAAAAAAAAAAAAgAH4AHgDgAQAAAAAAAPwAAAAAcANCFI0bAce6VNQ0A68X82z4oUWmGKyzSgNuYTFSAFoA";
+  const access_token = "CL2m8pSoMRIMQIEAQAAAYQIAAAAYGL-G5hQg5tDIHCjLvnwyFBh5ErR1WX4CGfMrc08gCm_ckVBXOjAAAABHAAAABAAAAAAAAAAAAIAAAAAAAAAAAAAgAH4AHgDgAQAAACAAAPwAAAAAcANCFCJjd9ICkWlQA3YbOWqXfiCvjoqHSgNuYTFSAFoA";
   if (req.body) {
     await axios.get(`https://api.hubapi.com/crm/v3/objects/deals/14998061332?properties=${joinpropertiesinarray}`,
       {
@@ -67,11 +46,28 @@ app.post('/submit', async (req, res) => {
       console.log("data", data.data.properties);
       // if (data.properties) {
         const hubspotdatafilledobject = data.data.properties;
+
+        // deleting properties not need to render ----------------
+        delete  hubspotdatafilledobject.createdate;
+        delete hubspotdatafilledobject.hs_lastmodifieddate;
+        delete hubspotdatafilledobject.hs_object_id;
+
         console.log('hubspotdatafilledobject', hubspotdatafilledobject)
+
+        //converting that values in array to pass to  index.ejs page to render ------
         const arrayofhubspotproperites = Object.values(hubspotdatafilledobject);
         console.log('arrayofhubspotproperites', arrayofhubspotproperites)
-      // }
-      res.send(JSON.stringify(data.data.properties));
+
+// Check if the length of the array matches the number of keys in the object
+if (Object.keys(json_object).length === arrayofhubspotproperites.length) {
+  Object.keys(json_object).forEach((key, index) => {
+    json_object[key] = arrayofhubspotproperites[index];
+  });
+
+  console.log("new",json_object);
+}
+
+ res.render('index', {  json_object });
     })
   }
 
@@ -79,10 +75,15 @@ app.post('/submit', async (req, res) => {
 
 app.get('/', async (req, res) => {
   const { associatedObjectId } = req.query
-  const responsedata = req.query
   // console.log(associatedObjectId);
-  // console.log(req.query)
-  const access_token = "CMmfg6OnMRIMAAEAQAAAYQIAAAAYGL-G5hQg5tDIHCjLvnwyFAZAsL8ar7K4Ql9cLHGATn5V17E8OjAAAABBAAAAAAAAAAAAAAAAAIAAAAAAAAAAAAAgAH4AHgDgAQAAAAAAAPwAAAAAcANCFI0bAce6VNQ0A68X82z4oUWmGKyzSgNuYTFSAFoA";
+  const initialjson = req.query
+  
+  if(initialjson){
+    initialdetail.addItem(initialjson);
+    console.log('Item added to the database');
+  }
+
+  const access_token = "CL2m8pSoMRIMQIEAQAAAYQIAAAAYGL-G5hQg5tDIHCjLvnwyFBh5ErR1WX4CGfMrc08gCm_ckVBXOjAAAABHAAAABAAAAAAAAAAAAIAAAAAAAAAAAAAgAH4AHgDgAQAAACAAAPwAAAAAcANCFCJjd9ICkWlQA3YbOWqXfiCvjoqHSgNuYTFSAFoA";
   if (associatedObjectId) {
     const schema = await axios.get("https://api.hubapi.com/crm/v3/schemas/deals", {
       headers: { Authorization: `Bearer ${access_token}` }
@@ -91,7 +92,7 @@ app.get('/', async (req, res) => {
     });
 
     if (!schema) {
-      return res.send('no schema found');
+      return res.status(200).send('no schema found');
     }
 
     // console.log("schema",schema.data.properties);
@@ -106,34 +107,6 @@ app.get('/', async (req, res) => {
             {
               "objectId": 989,
               "title": "CREATE PRELIM REQUEST",
-              // "link": "http://wattmonk.com",
-              // "properties": [
-              //   {
-              //     "label": "Address",
-              //     "dataType": "STRING",
-              //     "value": "rajghat"
-              //   },
-              //   {
-              //     "label": "chatid",
-              //     "dataType": "STRING",
-              //     "value": null
-              //   },
-              //   {
-              //     "label": "country",
-              //     "dataType": "STRING",
-              //     "value": null
-              //   },
-              //   {
-              //     "label": "dealname",
-              //     "dataType": "STRING",
-              //     "value": "first"
-              //   },
-              //   {
-              //     "label": "expecteddeliverydate",
-              //     "dataType": "STRING",
-              //     "value": null
-              //   },
-              // ]
               "actions": [
                 {
                   "type": "IFRAME",
@@ -142,189 +115,19 @@ app.get('/', async (req, res) => {
                   "uri": "https://fuzzy-guide-qpgwv7gpx9qcxw7-3000.app.github.dev/api/form",
                   "label": "Create prelim request"
                 },
-                // {
-                //   "type": "CONFIRMATION_ACTION_HOOK",
-                //   "confirmationMessage": "Are you sure you want to delete this ticket?",
-                //   "confirmButtonText": "Yes",
-                //   "cancelButtonText": "No",
-                //   "httpMethod": "DELETE",
-                //   "associatedObjectProperties": [
-                //     "protected_account"
-                //   ],
-                //   "uri": "https://example.com/tickets/245",
-                //   "label": "Delete"
-                // }
               ]
             },
           ],
-          // "settingsAction": {
-          //   "type": "IFRAME",
-          //   "width": 890,
-          //   "height": 748,
-          //   "uri": "https://example.com/settings-iframe-contents",
-          //   "label": "Settings"
-          // },
-          // "primaryAction": {
-          //   "type": "IFRAME",
-          //   "width": 890,
-          //   "height": 748,
-          //   "uri": "https://example.com/create-iframe-contents",
-          //   "label": "Create Ticket"
-          // }
         }
       )
     }
-
-    // const dealproperties = await axios.get(`https://api.hubapi.com/crm/v3/objects/deals/${associatedObjectId}?properties=address,dealname,chatid,status,expecteddeliverydate,country`, {
-    //   headers: { Authorization: `Bearer ${access_token}` }
-    // }).then(response => {
-    //   return response;
-    // }).catch(error => {
-    //       console.log(error)
-    // });
-    // console.log("dealproperties",dealproperties); 
-
-
-    // return res.send(
-    //   {
-    //     "results": [
-    //       {
-    //         "objectId": 989,
-    //         "title": "CREATE PRELIM REQUEST",
-    //         // "link": "http://wattmonk.com",
-    //         // "properties": [
-    //         //   {
-    //         //     "label": "Address",
-    //         //     "dataType": "STRING",
-    //         //     "value": "rajghat"
-    //         //   },
-    //         //   {
-    //         //     "label": "chatid",
-    //         //     "dataType": "STRING",
-    //         //     "value": null
-    //         //   },
-    //         //   {
-    //         //     "label": "country",
-    //         //     "dataType": "STRING",
-    //         //     "value": null
-    //         //   },
-    //         //   {
-    //         //     "label": "dealname",
-    //         //     "dataType": "STRING",
-    //         //     "value": "first"
-    //         //   },
-    //         //   {
-    //         //     "label": "expecteddeliverydate",
-    //         //     "dataType": "STRING",
-    //         //     "value": null
-    //         //   },
-    //         // ]
-    //         "actions": [
-    //           {
-    //             "type": "IFRAME",
-    //             "width": 890,
-    //             "height": 748,
-    //             "uri": "https://example.com/edit-iframe-contents",
-    //             "label": "Edit"
-    //           },
-    //           {
-    //             "type": "CONFIRMATION_ACTION_HOOK",
-    //             "confirmationMessage": "Are you sure you want to delete this ticket?",
-    //             "confirmButtonText": "Yes",
-    //             "cancelButtonText": "No",
-    //             "httpMethod": "DELETE",
-    //             "associatedObjectProperties": [
-    //               "protected_account"
-    //             ],
-    //             "uri": "https://example.com/tickets/245",
-    //             "label": "Delete"
-    //           }
-    //         ]
-    //       },
-    //       // {
-    //       //   "objectId": 990,
-    //       //   "title": "PERMITTING RESPONSE",
-    //       //   "link": "http://example.com/2",
-    //       //   "properties": [
-    //       //     {
-    //       //       "label": "Address",
-    //       //       "dataType": "STRING",
-    //       //       "value": "rajghat"
-    //       //     },
-    //       //     {
-    //       //       "label": "chatid",
-    //       //       "dataType": "STRING",
-    //       //       "value": null
-    //       //     },
-    //       //     {
-    //       //       "label": "country",
-    //       //       "dataType": "STRING",
-    //       //       "value": null
-    //       //     },
-    //       //     {
-    //       //       "label": "dealname",
-    //       //       "dataType": "STRING",
-    //       //       "value": "first"
-    //       //     },
-    //       //     {
-    //       //       "label": "expecteddeliverydate",
-    //       //       "dataType": "STRING",
-    //       //       "value": null
-    //       //     },
-    //       //   ]
-    //       //   },
-    //       // {
-    //       //   "objectId": 991,
-    //       //   "title": "PESTAMP RESPONSE",
-    //       //   "link": "http://example.com/2",
-    //       //   "properties": [
-    //       //     {
-    //       //       "label": "Address",
-    //       //       "dataType": "STRING",
-    //       //       "value": "rajghat"
-    //       //     },
-    //       //     {
-    //       //       "label": "chatid",
-    //       //       "dataType": "STRING",
-    //       //       "value": null
-    //       //     },
-    //       //     {
-    //       //       "label": "country",
-    //       //       "dataType": "STRING",
-    //       //       "value": null
-    //       //     },
-    //       //     {
-    //       //       "label": "dealname",
-    //       //       "dataType": "STRING",
-    //       //       "value": "first"
-    //       //     },
-    //       //     {
-    //       //       "label": "expecteddeliverydate",
-    //       //       "dataType": "STRING",
-    //       //       "value": null
-    //       //     },
-    //       //   ],
-    //       // },
-    //     ],
-    //       "settingsAction": {
-    //         "type": "IFRAME",
-    //         "width": 890,
-    //         "height": 748,
-    //         "uri": "https://example.com/settings-iframe-contents",
-    //         "label": "Settings"
-    //       },
-    //       "primaryAction": {
-    //         "type": "IFRAME",
-    //         "width": 890,
-    //         "height": 748,
-    //         "uri": "https://example.com/create-iframe-contents",
-    //         "label": "Create Ticket"
-    //       }
-    //     }
-    //   )
   }
 });
 
+app.get('/webhook', async (req,res) =>{
+  console.log("hitttted--------------")
+  console.log("req",req.query);
+})
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, () => console.log(`server is up!`));
